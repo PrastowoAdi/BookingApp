6dcacb4c87c9bfc35f6ba52d5b26e9b8ecf9e915
prastowoadi <sinaadi75@gmail.com>
2022-08-23 09:29:48 +0700

    Add homepage list Add Email list Footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,27 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import Propertylist from "../../components/propertyList/PropertyList";
 import "./home.css";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import "./home.css";
+import MailList from "../../components/mailList/MailList";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <Propertylist />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
     </div>
   );
 };
